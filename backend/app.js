faf560f1631f9f18ad298f1a65afa7951f414e19
Samarth Grover <samarthgrover8@gmail.com>
2024-03-03 02:21:33 +0000

    Added MongoDB to express app

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+
 const app = express();
 
 app.use(express.json());
-
 app.use('/api/user', require('./routes/userRoutes'));
+
+app.use(morgan("dev"));
+app.use(cors({ origin: true, credentials: true }));
 
 module.exports = app;
