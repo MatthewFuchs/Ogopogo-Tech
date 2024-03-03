1b6f21f68c9690171de5a58cc9428d46b12c9b3b
Samarth Grover <samarthgrover8@gmail.com>
2024-03-03 02:53:39 +0000

    Connection to cluster using MONGO_URI

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,12 @@ const cors = require('cors');
 
 const app = express();
 
+// MONGO_URI saved in .env file with cluster username and password
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("MongoDB connected")).catch((err) => console.log("MongoDB connection failed!", err));
+
 app.use(express.json());
 app.use('/api/user', require('./routes/userRoutes'));
 
