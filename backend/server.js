2cbc2316fed55050790c28f4f0aa97ba858fe397
itsdylantan <dylantan16@gmail.com>
2024-04-08 16:22:32 -0700

    Pushed port change for server

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
 const app = require('./app'); 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8002;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
