0717518e91c443a2d9e1e9000eb77285e1d073f6
jlgsl <88369347+jlgsl@users.noreply.github.com>
2024-04-12 12:04:54 -0700

    Update fileRoutes.js

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { fileUpload } = require('../controllers/fileController');
+const { fileUpload } = require('../controller/fileController');
 const router = express.Router();
 
 router.post('/upload/:assignmentId', fileUpload);
